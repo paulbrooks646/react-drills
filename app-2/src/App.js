6fbbc34c8898626e5214fb645172c9ec3e219368
paulbrooks646 <paulbrooks646@gmail.com>
2020-05-13 15:08:21 -0600

    app2

--- app-2/src/App.js
+++ app-2/src/App.js
@@ -1,26 +1,28 @@
-import React from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+export default class App extends Component {
+  constructor() {
+
+super()
+
+this.state = {
+ list: ["Splash Mountain", "Space Mountain", "The Matterhorn", "Thunder Mountain Railroad", "Indiana Jones"]
+}}
+
+render() {
+let disneyland = this.state.list.map((element, index) => {
+  return <h2 key={index}>{element}</h2>
+})
+  return(
+<div className="App">
+{disneyland}
+</div>
+  )
 }
 
-export default App;
+
+  
+}
