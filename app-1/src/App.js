903660b1ab5708d2a4140ee510f7bf6515eee587
paulbrooks646 <paulbrooks646@gmail.com>
2020-05-13 14:29:31 -0600

    app1

--- app-1/src/App.js
+++ app-1/src/App.js
@@ -1,26 +1,32 @@
-import React from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+export default class App extends Component {
+  constructor() {
+    super()
+
+    this.state = {
+     textBox : ""
+    }
+  }
+
+  handleChange(value) {
+    this.setState({textBox: value})
+  }
+
+  render() {
+
+    return(
+      <div className="App">
+      <input onChange={e => this.handleChange(e.target.value)} type="text"/>
+      <p>{this.state.textBox}</p>
     </div>
-  );
+    )
+  }
+
 }
 
-export default App;
+
+
+
