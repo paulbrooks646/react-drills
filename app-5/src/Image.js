e99599646d409519a67c9515131774f6d222c5c8
paulbrooks646 <paulbrooks646@gmail.com>
2020-05-13 18:55:44 -0600

    app5

---
+++ app-5/src/Image.js
@@ -0,0 +1,18 @@
+
+
+import React, {Component} from 'react';
+import './App.css';
+
+
+export default class Image extends Component {
+
+ 
+ render() {
+   return (
+     <div className="App">
+       <img src="https://cdn.britannica.com/55/174255-050-526314B6/brown-Guernsey-cow.jpg"/>
+       </div>
+   )
+ }
+
+}
